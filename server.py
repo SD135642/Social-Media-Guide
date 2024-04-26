0d8cca0c7176a6b8a173ddc399c242a8a2221e31
Aleksandra Dubrovina <be4mint.leaves@gmail.com>
2024-04-26 00:58:50 -0400

    Homepage and topics overview done

--- server.py
+++ server.py
@@ -12,21 +12,20 @@ topic_overviews = {
         "subtitle1": "Location",
         "subtitle2": "Niche",
         "subtitle3": "Age",
-        "image1": "",
-        "image2": "",
-        "image3": ""
+        "image1": "/static/images/img1.png",
+        "image2": "/static/images/img3.png",
+        "image3": "/static/images/img2.png"
     },
     "2":{
         "id": "2",
         "subbranch": 2,
-        "title": "Quality and Outreach",
-        "subtitle1": "Location",
-        "subtitle2": "Niche",
+        "title": "Personal Engagement",
+        "subtitle1": "Catchy Content",
+        "subtitle2": "Consistency",
         "subtitle3": "Age",
-        "image1": "",
-        "image2": "",
-        "image3": ""
-
+        "image1": "/static/images/img4.png",
+        "image2": "/static/images/img5.png",
+        "image3": "/static/images/img6.png"
     },
     "3": {
         "id": "3",
@@ -35,7 +34,7 @@ topic_overviews = {
         "subtitle1": "Diversification",
         "subtitle2": "Trends and Quality",
         "subtitle3": "Collaboration",
-        "image1": "",
+        "image1": "/static/images/img3.png",
         "image2": "",
         "image3": ""
 
@@ -46,63 +45,63 @@ topics = {
     "11":{
         "id": "11",
         "title": "Tailoring Content",
-        "text": "Text",
+        "text": "Compare these two images. What users are not going to look at these and follow these pages? Tap on the images to learn the answers.",
         "image1": "",
         "image2": ""
     },
-    "2":{
+    "12":{
         "id": "12",
         "title": "Tailoring Content",
-        "text": "Text",
+        "text": "Compare these two images. What users are not going to look at these and follow these pages? Tap on the images to learn the answers.",
         "image1": "",
         "image2": ""
     },
-    "3":{
+    "13":{
         "id": "13",
         "title": "Tailoring Content",
-        "text": "Text",
+        "text": "Compare these two images. What users are not going to look at these and follow these pages? Tap on the images to learn the answers.",
         "image1": "",
         "image2": ""
     },
-    "4":{
+    "21":{
         "id": "21",
         "title": "Quality and Outreach",
         "text": "Text",
         "image1": "",
         "image2": ""
     },
-    "5":{
+    "22":{
         "id": "22",
         "title": "Quality and Outreach",
         "text": "Text",
         "image1": "",
         "image2": ""
     },
-    "6":{
+    "23":{
         "id": "23",
         "title": "Quality and Outreach",
         "text": "Text",
         "image1": "",
         "image2": ""
     },
-    "7":{
+    "31":{
         "id": "31",
         "title": "Fostering Engagement",
-        "text": "Text",
+        "text": "Compare these two images. What do these kinds of engagement help the creator with? What would their followers think? Tap on the images to learn the answers.",
         "image1": "",
         "image2": ""
     },
-    "8":{
+    "32":{
         "id": "32",
         "title": "Fostering Engagement",
-        "text": "Text",
+        "text": "Compare these two images. What do these posts imply that draws the viewer's’ attention? Tap on the images to learn the answers.",
         "image1": "",
         "image2": ""
     },
-    "9":{
+    "33":{
         "id": "33",
         "title": "Fostering Engagement",
-        "text": "Text",
+        "text": "Look at these two images. What category does your account fall into? Choose and click on the right category on the left image. Do you have a content plan similar to the image on the right?",
         "image1": "",
         "image2": ""
     }
@@ -129,6 +128,11 @@ def display_topic(id=None):
     print("Here is the id: " + id)
     topic = topics[id]
     return render_template('topic.html', topic=topic)
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
 
 
 
